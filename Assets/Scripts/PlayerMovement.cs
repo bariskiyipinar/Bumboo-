8acc2451577300e42af61515d7b11adea42cb5d9
Baris <bariskiyipinar70@gmail.com>
2025-05-31 18:59:17 +0300

    Character Controller işlemleri tamamlandı.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer sp;
+
+    private Vector2 inputVector;
+    private bool isPressingW = false;
+    private bool wasPressingW = false;
+    private bool isPressingS = false;
+    private bool wasPressingS = false;
+
+    private void Start()
+    {
+        animator.SetBool("IsMoveX", false);
+        animator.SetBool("IsMoveZ", false);
+        animator.SetBool("IdleBack", false);
+    }
+
+    void Update()
+    {
+        inputVector.x = Input.GetAxisRaw("Horizontal");
+        inputVector.y = Input.GetAxisRaw("Vertical");
+        inputVector.Normalize();
+
+        // Sprite yönü
+        if (inputVector.x < 0f)
+            sp.flipX = true;
+        else if (inputVector.x > 0f)
+            sp.flipX = false;
+
+        isPressingW = Input.GetKey(KeyCode.W);
+        isPressingS = Input.GetKey(KeyCode.S);
+
+        // W býrakýldýðýnda IdleBack çalýþtýr
+        if (wasPressingW && !isPressingW && inputVector == Vector2.zero)
+        {
+            animator.SetBool("IdleBack", true);
+            animator.SetBool("IsIdle", false);
+        }
+        // S býrakýldýðýnda normal idle'a geç
+        else if (wasPressingS && !isPressingS && inputVector == Vector2.zero)
+        {
+            animator.SetBool("IdleBack", false);
+            animator.SetBool("IsIdle", true);
+        }
+
+
+        wasPressingW = isPressingW;
+        wasPressingS = isPressingS;
+    }
+
+    void FixedUpdate()
+    {
+        float moveX = inputVector.x * moveSpeed * Time.fixedDeltaTime;
+        float moveZ = inputVector.y * moveSpeed * Time.fixedDeltaTime;
+
+        Vector3 move = new Vector3(moveX, 0f, moveZ);
+        transform.position += move;
+
+        // Hareket kontrolleri
+        if (Input.GetKey(KeyCode.W))
+        {
+            animator.SetBool("IsMoveZ", true);
+            animator.SetBool("IsMoveX", false);
+            animator.SetBool("IdleBack", false);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            animator.SetBool("IsMoveZ", false);
+            animator.SetBool("IsMoveX", true);
+            animator.SetBool("IdleBack", false);
+        }
+        else if (inputVector.x != 0f)
+        {
+            animator.SetBool("IsMoveZ", false);
+            animator.SetBool("IsMoveX", true);
+            animator.SetBool("IdleBack", false);
+        }
+        else
+        {
+            animator.SetBool("IsMoveZ", false);
+            animator.SetBool("IsMoveX", false);
+      
+        }
+    }
+}
